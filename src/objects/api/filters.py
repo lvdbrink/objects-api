b72b519f0a1ecf8477d54eeee05341abb675646f
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-12-23 12:07:45 +0100

    :sparkles: Ref. #116 -- refactor api to use ObjectType instances instead of urls

--- src/objects/api/filters.py
+++ src/objects/api/filters.py
@@ -1,19 +1,38 @@
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 from django_filters import filters
 from vng_api_common.filtersets import FilterSet
-from vng_api_common.utils import get_help_text
 
-from objects.core.models import Object
+from objects.core.models import Object, ObjectType
 
 from .constants import Operators
 from .utils import display_choice_values_for_help_text, is_number
 from .validators import validate_data_attrs
 
 
+class ObjectTypeField(filters.ModelChoiceField):
+    def to_python(self, value):
+        if value in self.empty_values:
+            return None
+        try:
+            value = self.queryset.get_by_url(value)
+        except (ValueError, TypeError, self.queryset.model.DoesNotExist):
+            raise ValidationError(
+                self.error_messages["invalid_choice"], code="invalid_choice"
+            )
+        return value
+
+
+class ObjectTypeFilter(filters.ModelChoiceFilter):
+    field_class = ObjectTypeField
+
+
 class ObjectFilterSet(FilterSet):
-    type = filters.CharFilter(
-        field_name="object_type", help_text=get_help_text("core.Object", "object_type")
+    type = ObjectTypeFilter(
+        field_name="object_type",
+        help_text=_("Url reference to OBJECTTYPE in Objecttypes API"),
+        queryset=ObjectType.objects.all(),
     )
     data_attrs = filters.CharFilter(
         method="filter_data_attrs",
--- src/objects/api/serializers.py
+++ src/objects/api/serializers.py
@@ -1,9 +1,12 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
+from django.utils.encoding import smart_text
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeometryField
 
-from objects.core.models import Object, ObjectRecord
+from objects.core.models import Object, ObjectRecord, ObjectType
 
 from .validators import CorrectionValidator, IsImmutableValidator, JsonSchemaValidator
 
@@ -77,16 +80,37 @@ class HistoryRecordSerializer(serializers.ModelSerializer):
         }
 
 
+class ObjectTypeField(serializers.RelatedField):
+    default_error_messages = {
+        "does_not_exist": _("ObjectType with url={value} is not configured."),
+        "invalid": _("Invalid value."),
+    }
+
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_by_url(data)
+        except ObjectDoesNotExist:
+            self.fail("does_not_exist", value=smart_text(data))
+        except (TypeError, ValueError):
+            self.fail("invalid")
+
+    def to_representation(self, obj):
+        return obj.url
+
+
 class ObjectSerializer(serializers.HyperlinkedModelSerializer):
+    type = ObjectTypeField(
+        source="object_type",
+        queryset=ObjectType.objects.all(),
+        help_text=_("Url reference to OBJECTTYPE in Objecttypes API"),
+        validators=[IsImmutableValidator()],
+    )
     record = ObjectRecordSerializer(source="current_record")
 
     class Meta:
         model = Object
         fields = ("url", "type", "record")
-        extra_kwargs = {
-            "url": {"lookup_field": "uuid"},
-            "type": {"source": "object_type", "validators": [IsImmutableValidator()]},
-        }
+        extra_kwargs = {"url": {"lookup_field": "uuid"}}
         validators = [JsonSchemaValidator(), CorrectionValidator()]
 
     @transaction.atomic
